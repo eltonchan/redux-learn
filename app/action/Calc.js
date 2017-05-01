8d45ddb5357db51c2ee8032b47ad17547abeb692
yjh <eltonchan@live.com>
2017-05-01 16:58:36 +0800

    change

---
+++ app/action/Calc.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mac on 2017/5/1.
+ */
+
+export const Add = (text)=>({
+    type:'ADD',
+    text
+});
+
+export const Cut = (text)=>({
+    type:'CUT',
+    text
+});
---
+++ app/action/input.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mac on 2017/5/1.
+ */
+
+let addItem = (text)=>({
+    type:'ADD_ITEM',
+    text
+});
+export default addItem;
+
+
+
+
